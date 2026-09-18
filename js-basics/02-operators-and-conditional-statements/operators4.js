d2462f6c47c2de1dba1f86d687aa106a48b2b99a
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-18 22:48:30 +0530

    logical operators

---
+++ js-basics/02-operators-and-conditional-statements/operators4.js
@@ -0,0 +1,14 @@
+// logical operators
+
+let a = 5;
+let b = 5;
+let c = 6;
+
+// && --> both conditions should be true
+console.log(a == b && c > b)
+
+// || --> atleast one should be true
+console.log(a == b || b == c);
+
+// ! --> reverse the result 
+console.log(!(a == b));   

a4403fc66b7e0016fee385a0cf6f54652a6ecb5c
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-18 22:20:49 +0530

    unary operators

---
+++ js-basics/02-operators-and-conditional-statements/operators2.js
@@ -0,0 +1,11 @@
+// Unary Operators
+let a = 5;
+let b = 5;
+
+console.log("a =", a, "& b =", b);
+
+// a = a + 1;  // can also be written as a+=1;
+console.log(a++); // 5
+console.log(a); // 6
+
+console.log("++b =", ++b); // 6

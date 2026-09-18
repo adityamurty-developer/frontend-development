7d70cb70cad76898476dd910103c84049a9e47ed
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-18 20:40:53 +0530

    objects basics

---
+++ js-basics/01-variables-and-datatypes/object.js
@@ -0,0 +1,10 @@
+const employee = {
+    name : "Rohan",
+    salary : 2000000,
+    language : "Java Script",
+    isPass : true,
+};
+
+employee["salary"] = employee["salary"] + 100000
+
+console.log(employee["salary"]);

0b79069ac10dca2a141cc0427ac80cde2e156505
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-18 22:58:55 +0530

    conditional statement

---
+++ js-basics/02-operators-and-conditional-statements/conditional_statement_1.js
@@ -0,0 +1,30 @@
+// if statement 
+let age = 25;
+
+if(age >= 18){
+    console.log("You are eligible!")
+} 
+
+
+// if - else statement
+
+let x = 20;
+
+if(x%2 == 0){
+    console.log("Even number")
+} else {
+    console.log("odd number")
+}
+
+// else - if statement
+
+let a = 2;
+let b = 2;
+
+if(a === b){
+    console.log("both numbers are equal")
+} else if(b > a){
+    console.log("b is greater")
+} else {
+    console.log("a is greater")
+}

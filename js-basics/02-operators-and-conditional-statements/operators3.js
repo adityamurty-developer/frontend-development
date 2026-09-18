3945925d2ee8437f683c5f90798d429c6898feca
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-18 22:31:50 +0530

    comparison operators

---
+++ js-basics/02-operators-and-conditional-statements/operators3.js
@@ -0,0 +1,13 @@
+// Comparison operators
+
+let a = 5;
+let b = 5;
+
+console.log(a==b); // true
+console.log(a<=b); //true
+console.log(a>b); // false 
+
+let x = 2;
+let y = "2";
+
+console.log(x === y);  // both variables should be of same data type

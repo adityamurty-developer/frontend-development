6dc781adca2e412023bcc04f37c35892ca18a157
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-18 18:29:13 +0530

    variables and data types

---
+++ js-basics/01-variables-and-datatypes/first.js
@@ -0,0 +1,19 @@
+// variable and data type's
+
+let Name = "Rohan"  // string 
+console.log(Name)
+
+let age = 24;   // int
+console.log(age)
+
+let price = 99.99;  // float
+console.log(price)
+
+let a = null;
+console.log(a)
+
+let b = undefined;
+console.log(b)
+
+let isPossible = true;  // boolean
+let notPossible = false;    // boolean

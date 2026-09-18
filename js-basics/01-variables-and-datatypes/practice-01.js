cd8b17350d787971660445aba374b77e72ff04be
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-18 20:48:00 +0530

    practice problem 01

---
+++ js-basics/01-variables-and-datatypes/practice-01.js
@@ -0,0 +1,10 @@
+// create an object product which has title : Ball Pen, rating : 4, offer : 20, price : 50.
+
+const Product = {
+    title : "Ball Pen",
+    rating : 4, 
+    offer : 20,
+    price : 50 
+};
+
+console.log(Product)

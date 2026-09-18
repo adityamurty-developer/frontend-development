29d13321295ce67b459e7768b7755154d8e0ef26
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-18 20:54:54 +0530

    practice problem 02

---
+++ js-basics/01-variables-and-datatypes/practice-02.js
@@ -0,0 +1,13 @@
+// Create a const object --> profile to store your profile information.
+
+const profile = {
+    profile_name : "Rohan-developer",
+    posts : 195,
+    followers : "120k",
+    following : "1.3k",
+    total_likes : 2900,
+    total_comments : 967,
+    isVerified : true,
+};
+
+console.log(profile)

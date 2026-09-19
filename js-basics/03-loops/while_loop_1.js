1c3e1eb4e4e8c11567e57c3e3c182d642ca27e08
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-19 17:48:01 +0530

    while loop basics

---
+++ js-basics/03-loops/while_loop_1.js
@@ -0,0 +1,27 @@
+// basics of while loop
+
+let n = 5;
+let i = 1;
+
+while(i <= n){
+    console.log(i);
+    i++;
+}
+console.log("")
+
+// even printing
+console.log("even count problem")
+let nums = 20;
+let count = 2;
+
+// while(count <= nums){
+//     if(count % 2 == 0){
+//         console.log(count);
+//     }
+//     count++;
+// }
+
+while(count <= nums){
+    console.log(count);
+    count+=2;
+}

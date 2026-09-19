ed8290613909192cf7035dc00a3f26c2cf3675bd
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-19 20:32:30 +0530

    escape sequence

---
+++ js-basics/04-strings/string2.js
@@ -0,0 +1,11 @@
+// Escape Sequences
+
+console.log("Hello\nWorld");          // \n → New line
+
+console.log("Name:\tRohan");           // \t → Tab
+
+console.log("He said \"Hello\"");       // \" → Double quote
+
+console.log('It\'s JavaScript');       // \' → Single quote
+
+console.log("C:\\Users\\Rohan");       // \\ → Backslash

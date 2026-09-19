69ca5b2bcfbf01877866d7b40c3d0cb1935997ea
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-19 20:28:44 +0530

    strings basics

---
+++ js-basics/04-strings/string1.js
@@ -0,0 +1,12 @@
+// string indexing
+
+let str = "Hello, World";
+console.log(str[0], str[1]);
+
+// template literals
+
+let name = "Rohan";
+let age = 22;
+
+console.log(`My name is ${name} and I am ${age} years old.`); // this is template literals
+console.log("My name is", name, "and i am", age, "years old."); // this is normal multiple literals method

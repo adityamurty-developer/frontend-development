3641508ef2ce35bf92d70166add9db39a90d5c79
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-19 22:29:34 +0530

    array basics

---
+++ javascript-basics/05-arrays/arr1.js
@@ -0,0 +1,11 @@
+let nums = [10, 20, 30, 40, 50];
+
+// for(let i = 0; i < nums.length; i++){
+//     console.log(nums[i]);
+// }
+
+// same thing using for-of loop
+
+for(let value of nums){
+    console.log(value);
+}

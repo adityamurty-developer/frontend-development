745c333b32d35125496f5cecf07eb91430b44c7e
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-19 12:00:22 +0530

    for loop basics

---
+++ js-basics/03-loops/for_loop_1.js
@@ -0,0 +1,11 @@
+// basic for loop 
+
+console.log("Printing hello world 5 times")
+for(let i = 1; i <= 5; i++){
+    console.log("hello, World");
+}
+
+console.log("printing counting from 1 to 5")
+for(let i = 1; i <= 5; i++){
+    console.log(i);
+}

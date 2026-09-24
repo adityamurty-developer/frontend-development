e7865c55f51202ecba8c0a41701e2560a9252497
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-24 19:26:51 +0530

    functions basics

---
+++ javascript-basics/06-functions/functions_01.js
@@ -0,0 +1,19 @@
+function greet(name){
+    // parameter --> input
+    console.log(`hello, ${name}`);
+}
+
+greet("Aditya")  // argument
+greet("Rahul")
+
+console.log("");
+
+function addition(num1, num2){
+    
+    let sum = num1 + num2;  
+    return sum;
+}
+
+let result = addition(2, 3);
+
+console.log(`result is: ${result}`);
